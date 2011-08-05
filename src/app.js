405326ee5f72e45b58826571637f9a42f1ead69d
Michael Fong <theme97@gmail.com>
2011-08-05 08:25:22 +0000

    I totally did not just fail a merge

--- src/app.js
+++ src/app.js
@@ -14,11 +14,9 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'himitsu' }));
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: "keyboard cat" }));
+  app.use(express.session({ secret: 'himitsu' }));
   app.use(form({keepExtensions: true}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
